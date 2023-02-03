0ec867db4cb9ac31d03e35b163e24b635407ca6e
Nicolas Aruguete <naruguete@petersen.corp>
2023-02-03 12:48:49 -0300

    cambié la lógica en el login

--- pages/privado/js/main.js
+++ pages/privado/js/main.js
@@ -1,15 +1,43 @@
+// Simulación de usuarios en la base de datos //
+// Simulación de usuarios en la base de datos //
+const users = [
+    {username: 'nicoaru',
+    password: '1234'},
+    {username: 'juanperez',
+    password: '2345'},
+    {username: 'anaprada',
+    password: '3456'}
+]
+
+
+
 // Maneja el login //
 // Maneja el login //
 
 const loginButton = document.getElementById('loginButton')
 const usernameInput = document.getElementById('usernameInput')
+const passwordInput = document.getElementById('passwordInput')
+
 const login = (usernameValue) => {
     const username = usernameInput.value;
-    console.log("usernameValue: ", username)
-    sessionStorage.setItem('username', username);
-    location.assign("index.html")
+    const password = passwordInput.value;
+    console.log("usernameValue: ", username);
+
+    const user = users.find(elem => {
+        return elem.username === username;
+    })
+
+    console.log({user});
+
+    if (!user) alert('usuario inexistente')
+    else if (user.password === password) {
+        sessionStorage.setItem('username', username);
+        location.assign("index.html")
+    }
+    else alert('contraeña incorrecta')
+    
 }
-loginButton.onclick = () => {
+loginButton.onclick = (event) => {
     event.preventDefault();
     login(usernameInput);
 }
